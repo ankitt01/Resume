738f626f335552e58cbdca2cbb99c59217b73d70
ankitt01 <ankitrawat0102@gmail.com>
2022-02-04 14:08:15 +0530

    Education and Projects section done

---
+++ src/components/Resume.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import './styles/Resume.scss'
+
+function Resume() {
+  return (
+      <div className='resume'>
+          <h2 className='title'>Resume</h2>
+            <div className="education">
+                <h2>Education</h2>
+                <div className="tile">
+                    <p className="sub-title">Lovely Professional University, Jalandhar</p>
+                    <p className="year">2019 - 2023</p>
+                    <p className="desc">Bachelors of Technology in Computer Science <br /> CGPA: 7.5</p>
+                </div>
+                <div className="tile">
+                    <p className="sub-title">Delhi Public School, Ranipur</p>
+                    <p className="year">2017 - 2019</p>
+                    <p className="desc">Class XII CBSE Board <br /> Percentage: 80%</p>
+                </div>
+                <div className="tile">
+                    <p className="sub-title">Delhi Public School, Ranipur</p>
+                    <p className="year">2010 - 2017</p>
+                    <p className="desc">Class X CBSE Board <br /> CGPA: 9.2</p>
+                </div>
+            </div>
+
+            <div className="education">
+                <h2>Projects</h2>
+                <div className="tile">
+                    <p className="sub-title"> <a href="https://airbnb-clone-murex-xi.vercel.app/">Airbnb-Clone</a> </p>
+                    <p className="year">2021</p>
+                    <p className="desc">Used <span>Nextjs</span> framework to build the UI, and did the styling by the help of <span>Tailwind css</span> , Integrated <span>Mapbox</span> for displaying the map with the search Results, also integrated <span>Calendar</span> to pic the starting and ending date of your travel , and finally the <span>searching</span> destination functionality.</p>
+                </div>
+                <div className="tile">
+                    <p className="sub-title"><a href="https://ankitt01.github.io/Tesla-website/">Tesla Website Clone</a></p>
+                    <p className="year">2021</p>
+                    <p className="desc">This is a clone of <span>Tesla.com</span> website, built using <span>Reactjs</span> for the UI and <span>styled components</span> for styling of the webpage.</p>
+                </div>
+                <div className="tile">
+                    <p className="sub-title"><a href="https://ankitt01.github.io/Covid19-LandingPage/">Covid19 Data</a></p>
+                    <p className="year">2020</p>
+                    <p className="desc">Build a static webpage using <span>HTML</span>, <span>CSS</span> and <span>Js</span>. It displays the information about the CoronaVirus and statstics of the casualties. </p>
+                </div>
+            </div>
+
+            <div className="skills">
+                <h2>Skills</h2>
+            </div>
+      </div>
+  )
+}
+
+export default Resume;
+
+
