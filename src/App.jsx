5ef0a252e10361e41b7543ccc1ad22908edeeb23
ankitt01 <ankitrawat0102@gmail.com>
2022-02-04 11:37:06 +0530

    personal info done and resume section started

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.scss';
 import PersonalInfo from './components/PersonalInfo';
+import Resume from './components/Resume';
 
 function App() {
   return (
     <div className="App">
       <PersonalInfo />
+      <Resume />
     </div>
   );
 }
---
+++ src/components/Resume.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles/Resume.scss'
+
+function Resume() {
+  return (
+      <div className='resume'>
+          hello
+      </div>
+  )
+}
+
+export default Resume;
