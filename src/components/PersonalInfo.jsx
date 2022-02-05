73be31abb3fe2d6976ea8b24d467ab78c4818a69
ankitt01 <ankitrawat0102@gmail.com>
2022-02-06 00:50:06 +0530

    toggle function added

--- src/components/PersonalInfo.jsx
+++ src/components/PersonalInfo.jsx
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './styles/PersonalInfo.scss'
 import img from '../img/my-avatar.png'
 function PersonalInfo() {
+    const [clicked,setClicked] = useState(false);
+    function toggleContacts(){
+        if(clicked === false) {
+            setClicked(true);
+        }
+        else {
+            setClicked(false);
+        }
+    }
+
+    useEffect(() => {
+        function handleResize() {
+          if (window.innerWidth > 1000) {
+            setClicked(true);
+          } else {
+            setClicked(false);
+          }
+        }
+    
+        handleResize();
+    
+        window.addEventListener("resize", handleResize);
+    
+        return () => window.removeEventListener("resize", handleResize);
+      }, []);
   return (
     <div className='personal-info'>
         <div className="detail">
@@ -15,46 +40,52 @@ function PersonalInfo() {
                 </div>
             </div>
         </div>
-            <div className="down-arrow">
-                <i class="fas fa-chevron-down"></i>
-            </div>
-        <div className="contacts">
-            <div className="tiles">
-                <div className="contact">
-                    <div className="icon">
-                        <i class="far fa-envelope"></i>
-                    </div>
-                    <div>
-                        <h3 className="label">EMAIL</h3>
-                        <p className='value'>ankitrawat0102@gmail.com</p>
-                    </div>
-                </div>
-                <div className="contact">
-                    <div className="icon">
-                        <i class="fas fa-phone-alt"></i>
-                    </div>
-                    <div>
-                        <h3 className="label">PHONE</h3>
-                        <p className='value'>+91 8077904377</p>
-                    </div>
-                </div>
-                <div className="contact">
-                    <div className="icon">
-                        <i class="fas fa-map-marker-alt"></i>                
-                    </div>
-                    <div>
-                        <h3 className="label">LOCATION</h3>
-                        <p className='value'>Haridwar, Uttarakhand, India</p>
-                    </div>
-                </div>
-            </div>
-            
-            <div className="social-icons">
-                <i class="fab fa-facebook-f"></i>
-                <i class="fab fa-instagram"></i>
-                <i class="fab fa-github"></i>
-            </div>
+        
+        <div className="down-arrow" onClick={toggleContacts}>
+            <i class="fas fa-chevron-down"></i>
         </div>
+
+        <div className='contacts-container'>
+            {clicked && <div className={`contacts`}>
+                <div className="tiles">
+                    <div className="contact">
+                        <div className="icon">
+                            <i class="far fa-envelope"></i>
+                        </div>
+                        <div>
+                            <h3 className="label">EMAIL</h3>
+                            <p className='value'>ankitrawat0102@gmail.com</p>
+                        </div>
+                    </div>
+                    <div className="contact">
+                        <div className="icon">
+                            <i class="fas fa-phone-alt"></i>
+                        </div>
+                        <div>
+                            <h3 className="label">PHONE</h3>
+                            <p className='value'>+91 8077904377</p>
+                        </div>
+                    </div>
+                    <div className="contact">
+                        <div className="icon">
+                            <i class="fas fa-map-marker-alt"></i>                
+                        </div>
+                        <div>
+                            <h3 className="label">LOCATION</h3>
+                            <p className='value'>Haridwar, Uttarakhand, India</p>
+                        </div>
+                    </div>
+                </div>
+                
+                <div className="social-icons">
+                    <i class="fab fa-facebook-f"></i>
+                    <i class="fab fa-instagram"></i>
+                    <i class="fab fa-github"></i>
+                </div>
+            </div>}
+            
+        </div>
+
     </div>
   )
 }
