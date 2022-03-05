490137e8c0923ffe0be0603d5d651ac4cdf2b101
ankitt01 <ankitrawat0102@gmail.com>
2022-03-05 18:19:32 +0530

    Contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,18 @@
 import React from 'react'
-
+import './styles/Contact.scss'
 function Contact() {
   return (
     <div className="parent_container">
-        <h1 className='title'>Contact</h1>
+        <h1 className='title'>Contact Form</h1>
+
+        <form className="form">
+          <div className='input-div'>
+            <input type="text" placeholder='Full name' required />
+            <input type="text" placeholder='Email address' required/>
+          </div>
+          <textarea className='message' name="message" cols="10" rows="5" placeholder='Your Message' required/>
+          <button><i class="fa-solid fa-paper-plane"/>  Send Message</button>
+        </form>
     </div>
   )
 }
