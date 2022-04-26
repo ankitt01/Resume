f9171514d69726c536556d2bcd98abe017198557
ankitt01 <ankitrawat0102@gmail.com>
2022-04-26 13:32:00 +0530

    added code and project links

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -12,6 +12,8 @@ function Portfolio() {
       category: "Web Development",
       desc: "This project is a UI clone of Airbnb Website. It takes location from the user along with check-in and check-out dates of journey. Then fetches data according to the input from an API. Displays location in a map using MapBox. It is fully responsive for all device widths.",
       tech: [ "ReactJs", "NextJs", "Tailwind Css"],
+      github: "https://github.com/ankitt01/airbnb-clone",
+      live: "https://airbnb-clone-murex-xi.vercel.app/",
     },
     {
       img: "amazon.PNG",
@@ -19,20 +21,26 @@ function Portfolio() {
       category: "Web Development",
       desc: "This project is an Amazon UI clone in which the items are fetched from an API. It also has functionality of checking out and comes with Google Authentication. It is fully responsive for all device widths.",
       tech: ["ReactJs", "Redux", "NextJs", "Firebase"],
+      github: "https://github.com/ankitt01/amazon",
+      live: "https://amazon-nine-lyart.vercel.app/",
     },
     {
       img: "css-grid.PNG",
       title: "CSS Grids",
       category: "Web Design",
       desc: "Implementation of CSS Grid",
-      tech: ["CSS"]
+      tech: ["HTML", "CSS"],
+      github: "https://github.com/ankitt01/css-grid",
+      live: "https://ankitt01.github.io/css-grid/",
     },
     {
       img: "covid.PNG",
       title: "Covid 19 Landing Page",
       category: "Web Development",
       desc: "This project is a static landing page built using ReactJs which displays information about Covid19. Also fetches total number of cases.",
-      tech: ["HTML", "CSS", "JavaScript"]
+      tech: ["HTML", "CSS", "JavaScript"],
+      github: "https://github.com/ankitt01/Covid19-LandingPage",
+      live: "https://ankitt01.github.io/Covid19-LandingPage/",
     },
     {
       img: "cred.PNG",
@@ -40,6 +48,8 @@ function Portfolio() {
       category: "Web Development",
       desc: "This is a clone of CRED website's UI which is one of the best in startup world. Used technologies like intersection observor for animations. The website is responsive for all screen sizes",
       tech: ["ReactJs", "Tailwind CSS"],
+      github: "https://github.com/ankitt01/CRED-clone",
+      live: "https://credclone.netlify.app/",
     },
     {
       img: "crypto.PNG",
@@ -47,6 +57,8 @@ function Portfolio() {
       category: "Web Development",
       desc: "This is a cypto dashboard which consists of all cryptocurrency's prices in tabular form along with search functoinalities. Here user can create his own profile to bookmark certain crypto coins and keep track on their values. It also has data visualisation using chartjs library for every crypto coin. This website is responsive for all screen sizes",
       tech: ["ReactJs", "Tailwind CSS", "Firebase", "ChartJs"],
+      github: "https://github.com/ankitt01/Crypto-Tracker",
+      live: "https://crypto-tracker-website.netlify.app/",
     },
     {
       img: "tesla.PNG",
@@ -54,13 +66,17 @@ function Portfolio() {
       category: "Web Development",
       desc: "This is a clone of tesla's official site's UI. This was one of my very initial projects while learning react. Used reusable components for different sections of the website. This website is also responsive for all screen sizes.",
       tech: ["ReactJs", "Styled Components"],
+      github: "https://github.com/ankitt01/Tesla-website",
+      live: "https://ankitt01.github.io/Tesla-website/",
     },
     {
       img: "glint.PNG",
       title: "Static Landing Page",
       category: "Web Development",
       desc: "This is just a static landing page developed using HTML and Scss. This was among my very first projects in my web development career.",
-      tech: ["ReactJs", "Scss"],
+      tech: ["HTML", "Scss"],
+      github: "https://github.com/ankitt01/Landing-Page",
+      live: "https://ankitt01.github.io/Landing-Page",                      
     },
     
     {
@@ -68,7 +84,9 @@ function Portfolio() {
       title: "Traveling Landing Page",
       category: "Web Design",
       desc: "This is just a static landing page developed using HTML and CSS. This was among my very first projects in my web development career.",
-      tech: ["ReactJs", "CSS"],
+      tech: ["HTML", "CSS"],
+      github: "https://github.com/ankitt01/Travel-Agency-Webpage",
+      live: "https://ankitt01.github.io/Travel-Agency-Webpage/",
     },
 
 
--- src/components/ProjectDetail.js
+++ src/components/ProjectDetail.js
@@ -14,7 +14,9 @@ const ProjectDetail = (props) => {
         <NavLink to="/portfolio" className="linkp"><i class="fa fa-arrow-left" aria-hidden="true"></i>Back to Portfolio</NavLink>
           <div className="project-banner">
             <img  src={`/images/${project.img}`} alt="project-banner" />
-            {project.tech.map((tech) => (<button>{tech}</button> ) )}
+            {project.tech.map((tech) => (<button>{tech}</button> ) )} <br/> <br />
+            <button> <a href={project.github} target="_blank">Github Code</a> </button>
+            <button> <a href={project.live} target="_blank">Live Project Link</a> </button>
           </div>
           <p className='project-desc'>{project.desc}</p>
           <div className="slider">
